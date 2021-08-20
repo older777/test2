55c65c3fe9b76ee6a7629d32881a4223e43e25c1
older777 <kkkart@mail.ru>
2021-08-20 08:31:13 +0300

    fixing of types

--- test2.c
+++ test2.c
@@ -12,14 +12,14 @@
 #include <string.h>
 #include <stdlib.h>
 
-u_int8_t* byte_to_binary(u_int x){
-    static u_int8_t b[9];
+const char * byte_to_binary(u_int x){
+    static char * b;
+    b = malloc(9);
     b[0] = '\0';
     u_int z;
     for (z = 128; z > 0; z >>= 1)
     {
         strcat(b, ((x & z) == z) ? "1" : "0");
-
     }
     return b;
 }
@@ -28,7 +28,7 @@ u_int8_t* byte_to_binary(u_int x){
 int main() {
 
 	u_int8_t i = 0b11100001, n = 0, a = 0b11110000;
-	u_int8_t bufi[9], bufa[9], bufn[9];
+	char bufi[9], bufa[9], bufn[9];
 
 	memset(bufi, 0x00, 9);
 	memset(bufa, 0x00, 9);
@@ -42,7 +42,7 @@ int main() {
 			"a = %s\n"
 			"n = %s\n\n\n", bufi, bufa, bufn );
 
-	n = (a >> 1) & ~i;
+	n = (a > 127) ? n ^ i : ~ i;
 
 	strcpy(bufi, byte_to_binary(i));
 	strcpy(bufa, byte_to_binary(a));
@@ -53,4 +53,5 @@ int main() {
 			"n = %s\n\n\n", bufi, bufa, bufn );
 
 	return 0;
+
 }
